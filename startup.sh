b4a46da0ce404daa1ba5e2418fb7583ac10778e0
QP <qp@example.com>
2021-11-06 13:03:04 +0100

    adding a startup script

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# startup script
+# emacs
+# keyboard
+
+# backup script
+# https://ostechnix.com/backup-entire-linux-system-using-rsync/
+# sudo rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /mnt
