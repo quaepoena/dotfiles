614c5b9aa2261946de90f67d2634b93f30f86b1f
QP <quaepoena@example.com>
2024-08-21 11:40:52 +0200

    new setup.sh file

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ln bash_aliases ~/.bash_aliases
+ln dir-locals.el ~/.dir-locals.el
+ln emacs ~/.emacs
+ln inputrc ~/.inputrc
+ln xinitrc ~/.xinitrc
+
+# currently not in use
+# ln startup.sh ~/.startup.sh
+# ln xmodmaprc ~/.xmodmaprc
+# ln xmonad.hs ~/.xmonad.hs
